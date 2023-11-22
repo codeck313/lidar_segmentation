067c92aa8c61577b8a104f4e7f4a2c41fb5fea23
Saksham Sharma <23121752+codeck313@users.noreply.github.com>
2023-11-22 15:21:47 +0530

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -12,15 +12,15 @@ export ROS_HOSTNAME=localhost
 export ROS_MASTER_URI=http://localhost:11311
 
 #tmux file values
-echo -e "setw -g mouse on \n set -g mouse-select-pane on \n set -g mouse-resize-pane on \n set -g mouse-select-window on" >> /.tmux.config
+echo -e "setw -g mouse on \n set -g mouse-select-pane on \n set -g mouse-resize-pane on \n set -g mouse-select-window on" >> ~/.tmux.config
 
 # ROS installation
 ROS=/opt/ros/noetic/setup.bash
 source "$ROS"
 echo "$ECHO_PREFIX" "sourced ROS installation:" "$ROS"
 
-echo -e "function sdsd \n { \n current_path=`pwd` \n worskpace='_ws' \n source devel/setup.bash \n }" >> /.bashrc
-source /.bashrc
+echo -e "function sdsd \n { \n current_path=`pwd` \n worskpace='_ws' \n source devel/setup.bash \n }" >> ~/.bashrc
+source ~/.bashrc
 
 # workspace holding custom ROS packages
 workspace=/catkin_ws
@@ -29,4 +29,3 @@ echo "$ECHO_PREFIX" "sourced workspace:" "$workspace"
 
 echo "$ECHO_PREFIX" "call" "$@"
 exec "$@"
-
